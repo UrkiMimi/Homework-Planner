fd3b4866e4522a3c852f0aac2a11ca6d1e9cc54f
harbin <91095199+hk0569@users.noreply.github.com>
2023-04-06 11:00:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#init list
+list = []
+
 #functions
 def main_1():
     print('**Homework Planner** \n 1. Add to homework planner\n 2. View homework planner\n 3. Delete item from homework planner\n 4. Exit')
@@ -8,7 +11,10 @@ def main_1():
         print("Please enter a valid response")
         main_1()
 
-#init
+def add_list():
+    input=input('what do you want to add')
+    return input
+#init part 2 electric boogalo
 inp = main_1()
 if inp == '1':
     #Run Add to List
