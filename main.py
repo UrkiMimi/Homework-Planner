4b64cff3e6cd69509c896c7c436315bfa25dd61f
UrkiMii <74517821+UrkiMimi@users.noreply.github.com>
2023-04-06 10:52:56 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#functions
+def main_1():
+    print('**Homework Planner** \n 1. Add to homework planner\n 2. View homework planner\n 3. Delete item from homework planner\n 4. Exit')
+    inp = input('')
+    if inp == '1' or inp == '2' or inp == '3' or inp == '4':
+        return inp
+    else:
+        print("Please enter a valid response")
+        main_1()
+
+#init
+inp = main_1()
+if inp == '1':
+    #Run Add to List
+    print('h')
