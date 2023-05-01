746b31c66c0790bd11b1b5c60ec59a98ebdfda9b
UrkiMii <74517821+UrkiMimi@users.noreply.github.com>
2023-05-01 11:08:39 -0500

    wip commit of main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #init list
 list = []
+origlist = []
+alpha = []
 
 #functions
 def main_1():
-    print('**Homework Planner** \n 1. Add to homework planner\n 2. View homework planner\n 3. Delete item from homework planner\n 4. Exit')
+    print('**Homework Planner** \n 1. Add to homework planner\n 2. View homework planner\n 3. Delete item from homework planner\n 4. Sort List\n 5. Exit')
     inp = input('')
-    if inp == '1' or inp == '2' or inp == '3' or inp == '4':
+    if inp == '1' or inp == '2' or inp == '3' or inp == '4' or inp == '5':
         return inp
     else:
         print("Please enter a valid response")
@@ -20,7 +22,17 @@ def del_list():
     inp=int(input('Enter the number of what item you want to delete'))
     x=inp-1
     list.pop(x)
+    origlist.pop(x)
 
+def sort(list, origlist):
+    print('How would you like to sort the list?')
+    print(' 1. Sort by number\n 2. Sort by letter')
+    inp = input('')
+    if inp == '1':
+        return origlist
+    elif inp == '2':
+        return sorted(list)
+    
 #init part 2 electric boogalo
 while True:
     inp = main_1()
@@ -33,4 +45,6 @@ while True:
     elif inp == '3':
         del_list()
     elif inp == '4':
+        list = sort(list, origlist)
+    elif inp == '5':
         break
