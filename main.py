b6b61cbb1bcb1418f49b8b1ccef76992362dcff5
UrkiMii <74517821+UrkiMimi@users.noreply.github.com>
2023-05-02 10:38:22 -0500

    "sorting algorithm" type funny

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ origlist = []
 alpha = []
 
 #functions
+def printOutList():
+        print('Current Homework Planner')
+        for i in range(len(list)):
+            print(str(i + 1)+ ". " + list[i])
+        input('')
+
 def main_1():
     print('**Homework Planner** \n 1. Add to homework planner\n 2. View homework planner\n 3. Delete item from homework planner\n 4. Sort List\n 5. Exit')
     inp = input('')
@@ -16,7 +22,7 @@ def main_1():
 def add_list():
     inp=input('What do you want to add? ')
     inp2=input('What time is it due?')
-    return str(len(list)+1) + '. ' + str(inp) + " " + str(inp2)
+    return str(inp) + " " + str(inp2)
 
 def del_list():
     inp=int(input('Enter the number of what item you want to delete'))
@@ -26,7 +32,7 @@ def del_list():
 
 def sort(list, origlist):
     print('How would you like to sort the list?')
-    print(' 1. Sort by number\n 2. Sort by letter')
+    print(' 1. No sorting \n 2. Sort by letter')
     inp = input('')
     if inp == '1':
         return origlist
@@ -38,12 +44,13 @@ while True:
     inp = main_1()
     if inp == '1':
         list.append(add_list())
+        origlist = list
     elif inp == '2':
-        print('Current Homework Planner')
-        print('\n'.join(map(str, list)))
-        input('')
+        printOutList()
+        origlist = list
     elif inp == '3':
         del_list()
+        origlist = list
     elif inp == '4':
         list = sort(list, origlist)
     elif inp == '5':
